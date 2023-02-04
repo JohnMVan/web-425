32deb2f5f8e8b6f7ab9f3ae4baff5cf2e0d64c2b
JohnMVan <john.vanhessche@gmail.com>
2023-02-03 19:25:08 -0600

    Assignment 5.4

---
+++ week-5/in-n-out-booksp3/src/app/app.module.ts
@@ -0,0 +1,53 @@
+//Title:  app.module.ts
+//Author: John Vanhessche
+//Date 2 February 2023
+//Description:  TypeScript file for the app component.
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookListComponent } from './book-list/book-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+
+//Added for Ex 5.2
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { BookDetailsDialogComponent } from './book-details-dialog/book-details-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent,
+    ContactComponent,
+    AboutComponent,
+    BookDetailsDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+    MatDialogModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ week-5/in-n-out-booksp3/src/app/book-details-dialog/book-details-dialog.component.ts
@@ -0,0 +1,27 @@
+//Title:  book-details-dialog.component.ts
+//Author: John Vanhessche
+//Date 2 February 2023
+//Description:  TypeScript file for the book-details component.
+
+import { Component, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Inject } from '@angular/core';
+import { IBook } from '../book.interface';
+
+@Component({
+  selector: 'app-book-details-dialog',
+  templateUrl: './book-details-dialog.component.html',
+  styleUrls: ['./book-details-dialog.component.scss']
+})
+export class BookDetailsDialogComponent implements OnInit {
+
+  book: IBook;
+
+  constructor(private dialogRef: MatDialogRef<BookDetailsDialogComponent>, @Inject(MAT_DIALOG_DATA) data) {
+    this.book = data.book;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
